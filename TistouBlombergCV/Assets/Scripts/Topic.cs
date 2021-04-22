8c81a1a2a3e12b89eb60f9db3d3210151107c5db
Tistou Blomberg <tistou.blomberg@gmail.com>
2021-04-22 19:59:09 +0200

    Topic script created and testing in Map scene, buttons zoom in and out using virtual cameras when buttons pressed.

--- TistouBlombergCV/Assets/Scripts/Topic.cs
+++ TistouBlombergCV/Assets/Scripts/Topic.cs
@@ -2,39 +2,87 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using UnityEngine.UI;
 
 public class Topic : MonoBehaviour
 {
-    void turnOn()
+    public void Open()
     {
-        //Show title
-        //activate open-button
-    }
-    void turnOff()
-    {
-        //hide title
-        //deactivate open-button
-    }
-    void open()
-    {
-        //turn off
-        //turn on subtopics+description
-        //activate cam
-        //create back button
-    }
-    void close()
-    {
-        //turn off subtopics+description
-        //deactivate cam
-        //turn on
-    }
-    void createBackButton()
-    {
-
+        TurnOff();
+        TurnOnSubtopics();
+        TurnOnDescription();
+        CM.gameObject.SetActive(true);
+        CreateBackButton();
     }
 
-    public GameObject Title;
+    void Close()
+    {
+        TurnOffSubtopics();
+        TurnOffDescription();
+        CM.gameObject.SetActive(false);
+        TurnOn();
+    }
+    void TurnOnSubtopics()
+    {
+        foreach (Topic topic in Subtopics)
+        {
+            topic.gameObject.SetActive(true);
+        }
+    }
+    void TurnOffSubtopics()
+    {
+        foreach (Topic topic in Subtopics)
+        {
+            topic.gameObject.SetActive(false);
+        }
+    }
+    void TurnOnDescription()
+    {
+        Description.gameObject.SetActive(true);
+    }
+    void TurnOffDescription()
+    {
+        Description.gameObject.SetActive(false);
+    }
+    void TurnOn()
+    {
+        OpenButton.gameObject.SetActive(true);
+    }
+    void TurnOff()
+    {
+        OpenButton.gameObject.SetActive(false);
+    }
+    void CreateBackButton()
+    {
+        GameObject backButton = Instantiate(BackButtonPrefab, RootCanvas.transform);
+        backButton.GetComponent<Button>().onClick.AddListener(
+            () => OnClickBackButton(backButton)
+            );
+    }
+    void OnClickBackButton(GameObject backButton)
+    {
+        Close();
+        Destroy(backButton);
+    }
+    private void Awake()
+    {
+        ParentTopic = GetComponentInParent<Topic>();
+        foreach (Transform child in transform)
+        {
+            var topic = child.GetComponent<Topic>();
+            if(topic != null)
+            {
+                Subtopics.Add(topic);
+            }
+        }
+    }
+
+    public Button OpenButton;
+    public GameObject BackButtonPrefab;
     public CinemachineVirtualCameraBase CM;
-    public List<Topic> Subtopics;
+    public GameObject Description;
+    public GameObject RootCanvas;
     public Topic ParentTopic;
+    public List<Topic> Subtopics;
+    
 }

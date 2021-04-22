1f7fb10956d984ea3890b333f309db7cd0766de9
Tistou Blomberg <tistou.blomberg@gmail.com>
2021-04-22 12:27:25 +0200

    Scripts folder and Topic class planned with comments

---
+++ TistouBlombergCV/Assets/Scripts/Topic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Topic : MonoBehaviour
+{
+    void turnOn()
+    {
+        //Show title
+        //activate open-button
+    }
+    void turnOff()
+    {
+        //hide title
+        //deactivate open-button
+    }
+    void open()
+    {
+        //turn off
+        //turn on subtopics+description
+        //activate cam
+        //create back button
+    }
+    void close()
+    {
+        //turn off subtopics+description
+        //deactivate cam
+        //turn on
+    }
+    void createBackButton()
+    {
+
+    }
+
+    public GameObject Title;
+    public CinemachineVirtualCameraBase CM;
+    public List<Topic> Subtopics;
+    public Topic ParentTopic;
+}

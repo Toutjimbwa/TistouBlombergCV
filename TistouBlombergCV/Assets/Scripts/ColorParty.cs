8358c3245639d14879adda1151a78983a2898c6d
Tistou Blomberg <tistou.blomberg@gmail.com>
2021-05-05 16:22:43 +0200

    Leveldesign LAN-party and ColorParty.cs

---
+++ TistouBlombergCV/Assets/Scripts/ColorParty.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorParty : MonoBehaviour
+{
+    void Update()
+    {
+        Color = GetComponent<MeshRenderer>().material.color;
+        if(up) Color.b += increment;
+        else Color.b -= increment;
+        if (up) Color.g -= increment;
+        else Color.g += increment;
+        if (Color.b > 0.99f) up = false;
+        if (Color.b < 0.01f) up = true;
+        GetComponent<MeshRenderer>().material.color = Color;
+    }
+
+    public Color Color;
+    public float increment = 0.05f;
+
+    bool up = false;
+}
